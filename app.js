914bb3cf6fdd6a387e5472f8035eef549f481267
CASV331 <arturosandoval688@gmail.com>
2025-04-04 17:14:12 -0700

    actualizacion

--- app.js
+++ app.js
@@ -106,6 +106,4 @@ app.patch('/movies/:id', (req, res) => {
 })
 const PORT = process.env.PORT ?? 3000
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`)
-})
+module.exports = app
